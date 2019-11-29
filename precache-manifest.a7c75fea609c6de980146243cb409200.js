d4583c2d109d01726e105adcc074f05cea34f91e
HauptmanDev <MainVados@gmail.com>
2019-11-29 23:43:50 +0300

    Updates

---
+++ precache-manifest.a7c75fea609c6de980146243cb409200.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae195fd0cb2ceb87ed59f1168aa5bec",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "9576959d320a621b01be",
+    "url": "/Counter/static/css/main.104051c2.chunk.css"
+  },
+  {
+    "revision": "fb5847ff36b756b6afee",
+    "url": "/Counter/static/js/2.173e2b2c.chunk.js"
+  },
+  {
+    "revision": "9576959d320a621b01be",
+    "url": "/Counter/static/js/main.5108b741.chunk.js"
+  },
+  {
+    "revision": "cd6e87301d7e441c1fae",
+    "url": "/Counter/static/js/runtime~main.130d950d.js"
+  }
+]);
